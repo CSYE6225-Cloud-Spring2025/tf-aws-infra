d715b383253c3d032f82d16556bc6cb4b4402430
Sarthak Mallick <sarthak4242@gmail.com>
2025-03-18 16:46:10 -0400

    added ssh security key to AMI

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -1,9 +1,15 @@
+resource "aws_key_pair" "aws_ec2_key" {
+  key_name   = var.ec2_key_name
+  public_key = file(var.ec2_key_file)
+}
+
 resource "aws_instance" "web_application" {
   ami                         = var.ami_id
   instance_type               = var.ec2_instance_type
   associate_public_ip_address = true
   subnet_id                   = aws_subnet.public_subnet[1].id
   vpc_security_group_ids      = [aws_security_group.webapp_security_group.id]
+  key_name                    = aws_key_pair.aws_ec2_key.key_name
 
   disable_api_termination = false
   root_block_device {
